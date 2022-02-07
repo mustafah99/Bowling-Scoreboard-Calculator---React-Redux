394c4c0c9170fb19dc5b415ce555fe88259b55fb
Mustafa Said Helal <mustafasaidhelal@gmail.com>
2022-02-07 18:33:53 +0100

    Updated scoreSlice 'Max Possible' Logic

--- src/features/scores/scoreSlice.ts
+++ src/features/scores/scoreSlice.ts
@@ -17,24 +17,45 @@ export const scoreSlice = createSlice({
   reducers: {
     increaseScoreByAmount: (state, action: PayloadAction<number>) => {
       switch (action.payload) {
+        case 1:
+          state.value += action.payload;
+          state.maxPossible -= 29;
+          break;
         case 2:
+          state.value += action.payload;
           state.maxPossible -= 28;
           break;
+        case 3:
+          state.value += action.payload;
+          state.maxPossible -= 27;
+          break;
         case 4:
+          state.value += action.payload;
           state.maxPossible -= 26;
           break;
+        case 5:
+          state.value += action.payload;
+          state.maxPossible -= 25;
+          break;
         case 6:
+          state.value += action.payload;
           state.maxPossible -= 24;
           break;
+        case 7:
+          state.value += action.payload;
+          state.maxPossible -= 23;
+          break;
         case 8:
+          state.value += action.payload;
           state.maxPossible -= 22;
+          break;
+        case 9:
+          state.value += action.payload;
+          state.maxPossible -= 21;
           break;
         case 10:
           state.value += 30;
           break;
-        default:
-          state.value += action.payload;
-          state.maxPossible -= 10;
       }
     },
   },
